9a2938e4710cc1dfd5b3b5e1777a96e62bca5947
h11s-kpboon <kpboon@hitachi-ebworx.com>
2026-05-08 15:31:49 +0800

    n

--- sim.js
+++ sim.js
@@ -10,9 +10,9 @@ const API_LATEST  = 'https://api.api168168.com/pks/getLotteryPksInfo.do?lotCode=
 const API_HISTORY = 'https://api.api168168.com/pks/getPksHistoryList.do?lotCode=10037';
 
 const HIGHLIGHT        = 70;
-const MIN_N            = 7;
+const MIN_N            = 4;
 const CHECKS           = [2, 3, 4, 5, 6, 7];
-const BET_LADDER       = [33, 38, 79, 167, 349, 733, 1400];
+const BET_LADDER       = [11, 23, 48, 101, 212, 445, 935];
 const SPLIT_FROM       = 3;
 const STARTING_BALANCE = 2000;
 const HARD_CAP_LOSS    = 2000; // stop forever if balance hits 0
